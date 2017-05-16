be4162bdb2bf4182ef0b4027bfc6978958ae1e53
pandyroddick <dwstein@outlook.com>
2017-05-15 22:41:22 -0600

    Completed desktop and mobile header styles.

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,23 +1,4 @@
-import Person from './modules/Person';
-var $ = require('jquery');
-import Vue from 'vue';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
- payTaxes() {
-     console.log(this.name + " now owes $0 in taxes.")
- }
-}
+var mobileMenu = new MobileMenu();
 
-var john = new Adult("John Doe", "orange");
-john.greet();
-john.payTaxes();
-
-// $("h1").remove();
-// $("h2").remove();
-
-var app = new Vue({
-   el: "#testimonials",
-   data: {
-       message: "Howdy"
-   } 
-});
